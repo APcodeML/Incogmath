8b19ef22c928f8f04962dd80c1f125684bd0387e
Apnesh rawat <apnesh.rawat@gmail.com>
2021-09-10 12:20:14 +0530

    updated site

--- venv/Include/home.py
+++ venv/Include/home.py
@@ -1,15 +1,24 @@
-from flask import Flask, render_template,request
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template,request, redirect, url_for, session, abort
+from flask import flash,get_flashed_messages
 from datetime import datetime
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/incogmaths'
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-db = SQLAlchemy(app)
+import pymysql as MySQLdb
+import bcrypt
+import mysql.connector
 
+
+
+app = Flask(__name__)
+
+
+
+
+
+db= mysql.connector.connect(user='apnesh1' , password = 'Rohit123' , host = 'database-1.cbgajxq2f60f.us-east-2.rds.amazonaws.com', database = 'incogmat')
+'''
 class Problems(db.Model):
-    '''
-    sno, name phone_num, msg, date, email
-    '''
+    
+    #sno, name phone_num, msg, date, email
+    
     Serial_no = db.Column(db.Integer, primary_key=True)
     Question = db.Column(db.String(120), nullable=False)
     image= db.Column(db.LargeBinary)
@@ -18,15 +27,14 @@ class Problems(db.Model):
     Email = db.Column(db.String(20), nullable=False)
 
 class Contact(db.Model):
-    '''
-    sno, name phone_num, msg, date, email
-    '''
+       # sno, name phone_num, msg, date, email
+    
     sno = db.Column(db.Integer, primary_key=True)
     msg = db.Column(db.String(120), nullable=False)
     mobile = db.Column(db.String(12), nullable=False)
     date = db.Column(db.String(12))
     email = db.Column(db.String(20), nullable=False)
-
+'''
 
 
 @app.route("/")
@@ -63,5 +71,66 @@ def contact():
         db.session.commit()
 
     return render_template('contact.html')
+@app.route("/admin" , methods = ['GET' , 'POST'])
+def admin():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password'].encode('utf-8')
 
-app.run(debug=True)
+        curl= db.cursor(MySQLdb.cursors.DictCursor)
+        curl.execute("SELECT * FROM users WHERE email=%s",(email,))
+        #print(curl)
+        user=curl.fetchone()
+        curl.close()
+        if user == None:
+            return "User Not Found!!!"
+        if len(user)>0:
+            if bcrypt.hashpw(password,user[1].encode('utf-8')) == user[1].encode('utf-8'):
+                session['email'] = user[0]
+                session['authenticate'] = True
+                return redirect(url_for("dashboard"))
+            else:
+                return "Error password and email does not match"
+
+    else:
+        return render_template("index.html")
+@app.route("/dashboard")
+def dashboard():
+    print(session.get("authenticate"))
+    if session.get("authenticate"):
+        return render_template("main.html" ,x = "I am your question!")
+    return redirect(url_for("admin"))
+
+@app.route("/signup",methods=['GET','POST'])
+def signup():
+    if request.method == 'GET':
+        return render_template("signup.html")
+    else:
+      #  name = request.form['name']
+        email = request.form['email']
+        password = request.form['password'].encode('utf-8')
+        hash_password = bcrypt.hashpw(password,bcrypt.gensalt())
+
+        cur = db.cursor()
+        cur.execute("INSERT INTO users (email, password) VALUES (%s,%s)" , (email,hash_password))
+        db.commit()
+        session['email'] = request.form['email']
+        return redirect(url_for('home'))
+
+@app.route("/apnesh",methods=['GET','POST'])
+def apnesh():
+    if request.method == "GET":
+        return render_template("main.html" ,x = "I am your question!")
+    else:
+        qid = request.form['qid']
+
+        curl = db.cursor(MySQLdb.cursors.DictCursor)
+        curl.execute("SELECT img_txt FROM question WHERE q_id = %s",(qid,))
+        x=curl.fetchone()
+        curl.close()
+        return render_template("main.html", x=x)
+
+
+if __name__=="__main__":
+    app.secret_key ='\xd9\x0f\x8c\xfb\xbf\x914"Z}L\xfcs\rG\x04\xc8"\'\x9d\xc7\xe5\x11n'
+    app.run(debug=True)
---
+++ venv/Include/static/scripts/script.js
@@ -0,0 +1,7 @@
+tinymce.init({
+    selector: '#mytextarea',
+    height: 350,
+    menubar:'insert edit format',
+    toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | a11ycheck ltr rtl | showcomments addcomment',
+    plugins: "insertdatetime table wordcount image"
+  });
